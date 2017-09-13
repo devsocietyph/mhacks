e102428401e29fdb023d665aaa470dad538ba0a2
Chia Amisola <chiamisola@gmail.com>
2017-09-13 14:52:30 +0800

    Add files

--- js/main.js
+++ js/main.js
@@ -4,3 +4,32 @@
 function entryFilter(type){
     
 }
+
+
+/* $(document).ready(function() {
+    $('#navigation').click(function(e) {  
+       $('#navigation').fadeOut();
+    });
+    
+    $('#navigation a').click(function(e) {  
+       $('#navigation').fadeOut();
+    });
+    
+});
+
+function hamburger(){
+    $("#navigation").fadeIn();
+} */
+
+$(function () {
+    $('a').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body,.container').animate({ scrollTop: target.offset().top }, 1000);
+                return false;
+            }
+        }
+    });
+});
